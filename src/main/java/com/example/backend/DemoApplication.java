6bd383fc0563a90d401f33be12c90b9ae6cdb80c
JungHan-Seok <jhstone034679@gmail.com>
2025-07-06 21:54:25 +0900

    회원가입/로그인 시스템

--- src/main/java/com/example/backend/DemoApplication.java
+++ src/main/java/com/example/backend/DemoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication {  // 혹은 BackendApplication 이름 상관없음
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
